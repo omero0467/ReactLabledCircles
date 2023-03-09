99e6f41f2db79bb258af6de9c97c0f9ac6e7b9b8
omer evron <omerevron011@gmail.com>
2023-03-09 17:49:20 +0200

    reset btn

--- circles-data-vis/src/Chart.jsx
+++ circles-data-vis/src/Chart.jsx
@@ -29,6 +29,19 @@ const Chart = () => {
       let focus = root;
       let view;
 
+      function handleReset (event) {
+        event.preventDefault()
+        zoom(event,root)
+      }
+
+      const zoomConfig = d3
+        .zoom()
+        .extent([
+          [0, 0],
+          [width, height],
+        ])
+        .on("zoom", zoom);
+
       //Color Assigning function
       const color = d3
         .scaleLinear()
@@ -42,11 +55,13 @@ const Chart = () => {
       const svg = d3
         .select("#chart")
         .append("svg")
-        .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+        .attr("height", height)
+        .attr("width", width)
+        // .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
         .style("display", "block")
         .style("background", color(0))
-        .style("cursor", "pointer")
-        .on("zoom", zoom);
+        .style("cursor", "pointer");
+      // .call(zoomConfig)
 
       const tooltip = d3.select("#tooltip");
       tooltip.style("left", "100%");
@@ -54,8 +69,7 @@ const Chart = () => {
       const innerSpace = svg
         .append("g")
         .classed("inner_space", true)
-        .attr("transform", `translate(0,0)`)
-        .call();
+        .call(zoomConfig);
 
       innerSpace
         .append("rect")
@@ -63,13 +77,15 @@ const Chart = () => {
         .attr("height", height)
         .attr("fill", "transparent");
       //Circles Initial
-      const node = svg
+      const node = innerSpace
         .append("g")
+        .attr("transform", `translate(${width / 2},${height / 2})`)
         .selectAll("circle")
         .data(root.descendants().slice(1))
         .join("circle")
         .attr("fill", (d) => (d.children ? color(d.depth) : "white"))
         .attr("pointer-events", (d) => (!d.children ? "none" : null))
+        // .call(zoomConfig)
         .on("mouseover", function () {
           d3.select(this).attr("stroke", "#000");
         })
@@ -81,8 +97,9 @@ const Chart = () => {
           (event, d) => focus !== d && (zoom(event, d), event.stopPropagation())
         );
 
-      const label = svg
+      const label = innerSpace
         .append("g")
+        .attr("transform", `translate(${width / 2},${height / 2})`)
         .style("font", "10px sans-serif")
         .attr("pointer-events", "none")
         .attr("text-anchor", "middle")
@@ -92,12 +109,19 @@ const Chart = () => {
         .style("fill-opacity", (d) => (d.parent === root ? 1 : 0))
         .style("display", (d) => (d.parent === root ? "inline" : "none"))
         .text((d) => "Group Name");
+
+      const resetBtn = d3.select("#chart")
+      .append("button")
+      .text('Reset')
+      .on('click',handleReset)
+      
       //Display Initial
 
       zoomTo([root.x, root.y, root.r * 2]);
 
       //Zoom transition
       function zoomTo(v) {
+        console.log("🚀 ~ file: Chart.jsx:113 ~ zoomTo ~ v:", v);
         const k = width / v[2];
 
         view = v;
@@ -118,13 +142,15 @@ const Chart = () => {
 
         focus = d;
 
-        const transition = svg
+        innerSpace.attr("transform", event.transform);
+
+        const transition = innerSpace
           .transition()
-          .duration(event.altKey ? 7500 : 750)
-          .tween("zoom", (d) => {
-            const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-            return (t) => zoomTo(i(t));
-          });
+          .duration(event.altKey ? 7500 : 750);
+        // .tween("zoom", (d) => {
+        //   const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+        //   return (t) => zoomTo(i(t),event.transform);
+        // });
 
         label
           .filter(function (d) {
